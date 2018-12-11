0bfe3203b3a442befde45b40e3af20533158cbbc
kelemantamas <kelemantamas@users.noreply.github.com>
2018-12-11 09:47:49 +0200

    main screen modified splash screen added just for project

---
+++ app/src/main/java/sapadvrtisrproj/ms/sapientia/ro/allam4/SplashActivity.java
@@ -0,0 +1,28 @@
+package sapadvrtisrproj.ms.sapientia.ro.allam4;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(main);
+                finish();
+            }
+        },SPLASH_DISPLAY_LENGTH);
+    }
+
+
+}
